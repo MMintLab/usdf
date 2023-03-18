0774a6894496ceb025e327c362cacb6d522bfa19
Mark Van der Merwe <markvdm@umich.edu>
2023-03-18 13:02:36 -0400

    Rotating render setup.

--- scripts/render_mug.py
+++ scripts/render_mug.py
@@ -8,12 +8,13 @@ from usdf import utils
 from usdf.render_utils import depth_to_pointcloud
 from vedo import Plotter, Points, Mesh
 
-mesh_tri = trimesh.load("out/meshes/shapenet/mugs_proc/1a1c0a8d4bad82169f0594e65f756cf5.obj")
-mesh = pyrender.Mesh.from_trimesh(mesh_tri)
+scene = pyrender.Scene()
 
 # Add mesh to the scene.
-scene = pyrender.Scene()
-scene.add(mesh)
+mesh_tri: trimesh.Trimesh = trimesh.load("out/meshes/shapenet/mugs_proc/1a1c0a8d4bad82169f0594e65f756cf5.obj")
+mesh = pyrender.Mesh.from_trimesh(mesh_tri)
+mesh_node = pyrender.Node(mesh=mesh, matrix=np.eye(4))
+scene.add_node(mesh_node)
 
 # Add camera to the scene.
 yfov = np.pi / 3.0
@@ -27,23 +28,20 @@ scene.add(camera, pose=camera_pose)
 light = pyrender.SpotLight(color=np.ones(3), intensity=3.0, innerConeAngle=np.pi / 16.0, outerConeAngle=np.pi / 6.0)
 scene.add(light, pose=camera_pose)
 
-# Render the scene.
-r = pyrender.OffscreenRenderer(512, 512)
-color, depth = r.render(scene)
+for angle in np.linspace(0, 2 * np.pi, 10):
+    object_pose = np.eye(4)
+    object_pose[:3, :3] = tf3d.euler.euler2mat(0, 0, angle, axes="sxyz")
+    scene.set_pose(mesh_node, object_pose)
 
-# plt.figure()
-# plt.subplot(1, 2, 1)
-# plt.axis('off')
-# plt.imshow(color)
-# plt.subplot(1, 2, 2)
-# plt.axis('off')
-# plt.imshow(depth, cmap=plt.cm.gray_r)
-# plt.show()
+    # Render the scene.
+    r = pyrender.OffscreenRenderer(512, 512)
+    color, depth = r.render(scene)
 
-# Convert depth to pointcloud.
-pointcloud = depth_to_pointcloud(depth, yfov)
-pointcloud = utils.transform_pointcloud(pointcloud, camera_pose)
+    # Convert depth to pointcloud.
+    pointcloud = depth_to_pointcloud(depth, yfov)
+    pointcloud = utils.transform_pointcloud(pointcloud, camera_pose)
 
-# Plot pointcloud.
-vedo_plt = Plotter()
-vedo_plt.at(0).show(Points(pointcloud[:, :3]), Mesh([mesh_tri.vertices, mesh_tri.faces]))
+    # Plot pointcloud.
+    vedo_plt = Plotter()
+    mesh_tri_rot = mesh_tri.copy().apply_transform(object_pose)
+    vedo_plt.at(0).show(Points(pointcloud[:, :3]), Mesh([mesh_tri_rot.vertices, mesh_tri_rot.faces]))
