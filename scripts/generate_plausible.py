05d89ad832cb3352086a307340c8aecf88a60758
Mark Van der Merwe <markvdm@umich.edu>
2023-03-22 18:24:03 -0400

    Better progress bar

--- scripts/generate_plausible.py
+++ scripts/generate_plausible.py
@@ -20,6 +20,7 @@ def generate_plausible(dataset_cfg: dict, split: str, vis: bool = True):
     # TODO: Move to config.
     N = 2000  # Number of points to use in partial pointcloud.
     B = 32  # Batch size for CHSEL.
+    N_angles = 8  # Number of angles each mesh is rendered from.
     use_cached_sdf: bool = True
     d = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -37,99 +38,103 @@ def generate_plausible(dataset_cfg: dict, split: str, vis: bool = True):
     partials_fns = os.listdir(partials_dir)
 
     # Apply CHSEL on each mesh in dataset to attempt registration to partial view.
-    for mesh_fn in tqdm(mesh_fns):
-        mesh_fn_full = os.path.join(meshes_dir, mesh_fn)
-        mesh_tri: trimesh.Trimesh = trimesh.load(mesh_fn_full)
-        obj = pv.MeshObjectFactory(mesh_fn_full)
-        sdf = pv.MeshSDF(obj)
-        if use_cached_sdf:
-            sdf = pv.CachedSDF(os.path.splitext(mesh_fn)[0], resolution=0.02,
-                               range_per_dim=obj.bounding_box(padding=0.1), gt_sdf=sdf, device=d)
+    with tqdm(total=len(mesh_fns) * len(partials_fns) * N_angles) as pbar:
+        for mesh_fn in mesh_fns:
+            mesh_fn_full = os.path.join(meshes_dir, mesh_fn)
+            mesh_tri: trimesh.Trimesh = trimesh.load(mesh_fn_full)
+            obj = pv.MeshObjectFactory(mesh_fn_full)
+            sdf = pv.MeshSDF(obj)
+            if use_cached_sdf:
+                sdf = pv.CachedSDF(os.path.splitext(mesh_fn)[0], resolution=0.02,
+                                   range_per_dim=obj.bounding_box(padding=0.1), gt_sdf=sdf, device=d)
 
-        results = dict()
-        for partial_fn in partials_fns:
-            partial_views_dirs = os.listdir(os.path.join(partials_dir, partial_fn))
-            partial_views_dirs.sort(key=lambda x: float(x))
-            source_mesh_fn = os.path.join(meshes_dir, partial_fn + ".obj")
-            source_mesh = trimesh.load(source_mesh_fn)
+            results = dict()
+            for partial_fn in partials_fns:
+                partial_views_dirs = os.listdir(os.path.join(partials_dir, partial_fn))
+                partial_views_dirs.sort(key=lambda x: float(x))
+                source_mesh_fn = os.path.join(meshes_dir, partial_fn + ".obj")
+                source_mesh = trimesh.load(source_mesh_fn)
 
-            results[partial_fn] = dict()
+                results[partial_fn] = dict()
 
-            for partial_view_dir in partial_views_dirs:
-                partial_angle_dir = os.path.join(partials_dir, partial_fn, partial_view_dir)
-                pointcloud = utils.load_pointcloud(os.path.join(partial_angle_dir, "pointcloud.ply"))[:, :3]
-                free_pointcloud = utils.load_pointcloud(os.path.join(partial_angle_dir, "free_pointcloud.ply"))[:, :3]
-                angle = mmint_utils.load_gzip_pickle(os.path.join(partial_angle_dir, "info.pkl.gzip"))["angle"]
+                for partial_view_dir in partial_views_dirs:
+                    partial_angle_dir = os.path.join(partials_dir, partial_fn, partial_view_dir)
+                    pointcloud = utils.load_pointcloud(os.path.join(partial_angle_dir, "pointcloud.ply"))[:, :3]
+                    free_pointcloud = utils.load_pointcloud(os.path.join(partial_angle_dir, "free_pointcloud.ply"))[:,
+                                      :3]
+                    angle = mmint_utils.load_gzip_pickle(os.path.join(partial_angle_dir, "info.pkl.gzip"))["angle"]
 
-                # Downsample partial pointcloud.
-                np.random.shuffle(pointcloud)
-                pointcloud = torch.from_numpy(pointcloud[:N]).to(d).float()
+                    # Downsample partial pointcloud.
+                    np.random.shuffle(pointcloud)
+                    pointcloud = torch.from_numpy(pointcloud[:N]).to(d).float()
 
-                # Filter free points to lie in ball around object.
-                free_pointcloud = free_pointcloud[np.linalg.norm(free_pointcloud, axis=1) < 1.1]
-                np.random.shuffle(free_pointcloud)
-                free_pointcloud = torch.from_numpy(free_pointcloud[:N]).to(d).float()
+                    # Filter free points to lie in ball around object.
+                    free_pointcloud = free_pointcloud[np.linalg.norm(free_pointcloud, axis=1) < 1.1]
+                    np.random.shuffle(free_pointcloud)
+                    free_pointcloud = torch.from_numpy(free_pointcloud[:N]).to(d).float()
 
-                # Setup semantics of pointcloud.
-                sem_sdf = np.zeros((N,))
-                sem_free = [chsel.SemanticsClass.FREE] * N
+                    # Setup semantics of pointcloud.
+                    sem_sdf = np.zeros((N,))
+                    sem_free = [chsel.SemanticsClass.FREE] * N
 
-                # Combine pointclouds to send to CHSEL.
-                points = torch.cat([pointcloud, free_pointcloud], dim=0)
-                semantics = sem_sdf.tolist() + sem_free
+                    # Combine pointclouds to send to CHSEL.
+                    points = torch.cat([pointcloud, free_pointcloud], dim=0)
+                    semantics = sem_sdf.tolist() + sem_free
 
-                # Visualize the points being used.
-                if vis:
-                    plt = Plotter()
-                    # Rotate source mesh here based on angle.
-                    source_mesh_angle = source_mesh.copy().apply_transform(
-                        trimesh.transformations.rotation_matrix(angle, [0, 0, 1]))
-                    plt.at(0).show(Points(pointcloud.cpu().numpy(), c="green"),
-                                   Points(free_pointcloud.cpu().numpy(), c="blue", alpha=0.2),
-                                   Mesh([source_mesh_angle.vertices, source_mesh_angle.faces]))
+                    # Visualize the points being used.
+                    if vis:
+                        plt = Plotter()
+                        # Rotate source mesh here based on angle.
+                        source_mesh_angle = source_mesh.copy().apply_transform(
+                            trimesh.transformations.rotation_matrix(angle, [0, 0, 1]))
+                        plt.at(0).show(Points(pointcloud.cpu().numpy(), c="green"),
+                                       Points(free_pointcloud.cpu().numpy(), c="blue", alpha=0.2),
+                                       Mesh([source_mesh_angle.vertices, source_mesh_angle.faces]))
 
-                # Initialize transforms to rotations around z.
-                euler_angles = torch.zeros((B, 3), device=d)
-                euler_angles[:, 2] = torch.linspace(0.0, 2 * np.pi, B + 1, device=d)[:-1]
-                random_init_tsf = pk.Transform3d(pos=torch.zeros((B, 3), device=d),
-                                                 rot=pk.euler_angles_to_matrix(euler_angles, "XYZ"), device=d)
-                random_init_tsf = random_init_tsf.get_matrix()
+                    # Initialize transforms to rotations around z.
+                    euler_angles = torch.zeros((B, 3), device=d)
+                    euler_angles[:, 2] = torch.linspace(0.0, 2 * np.pi, B + 1, device=d)[:-1]
+                    random_init_tsf = pk.Transform3d(pos=torch.zeros((B, 3), device=d),
+                                                     rot=pk.euler_angles_to_matrix(euler_angles, "XYZ"), device=d)
+                    random_init_tsf = random_init_tsf.get_matrix()
 
-                registration = chsel.CHSEL(sdf, points, semantics, qd_iterations=100, free_voxels_resolution=0.02)
-                res, all_solutions = registration.register(iterations=15, batch=B, initial_tsf=random_init_tsf)
+                    registration = chsel.CHSEL(sdf, points, semantics, qd_iterations=100, free_voxels_resolution=0.02)
+                    res, all_solutions = registration.register(iterations=15, batch=B, initial_tsf=random_init_tsf)
 
-                if vis:
-                    # print the sorted RMSE for each iteration
-                    print(torch.sort(registration.res_history[-1].rmse).values)
+                    if vis:
+                        # print the sorted RMSE for each iteration
+                        print(torch.sort(registration.res_history[-1].rmse).values)
 
-                    # res.RTs.R, res.RTs.T, res.RTs.s are the similarity transform parameters
-                    # get 30 4x4 transform matrix for homogeneous coordinates
-                    world_to_link = chsel.solution_to_world_to_link_matrix(registration.res_history[-1])
-                    link_to_world = world_to_link.inverse()
+                        # res.RTs.R, res.RTs.T, res.RTs.s are the similarity transform parameters
+                        # get 30 4x4 transform matrix for homogeneous coordinates
+                        world_to_link = chsel.solution_to_world_to_link_matrix(registration.res_history[-1])
+                        link_to_world = world_to_link.inverse()
 
-                    # Transform meshes based on results.
-                    mesh_estimates = []
-                    for i in range(link_to_world.shape[0]):
-                        mesh_tri_est = mesh_tri.copy().apply_transform(link_to_world[i].cpu().numpy())
-                        mesh_estimates.append(Mesh([mesh_tri_est.vertices, mesh_tri_est.faces], alpha=0.5))
+                        # Transform meshes based on results.
+                        mesh_estimates = []
+                        for i in range(link_to_world.shape[0]):
+                            mesh_tri_est = mesh_tri.copy().apply_transform(link_to_world[i].cpu().numpy())
+                            mesh_estimates.append(Mesh([mesh_tri_est.vertices, mesh_tri_est.faces], alpha=0.5))
 
-                    plt = Plotter()
-                    source_mesh_angle = source_mesh.copy().apply_transform(
-                        trimesh.transformations.rotation_matrix(angle, [0, 0, 1]))
-                    plt.at(0).show(
-                        Points(pointcloud.cpu().numpy(), c="green", alpha=0.2),
-                        Points(free_pointcloud.cpu().numpy(), c="blue", alpha=0.2),
-                        Mesh([source_mesh_angle.vertices, source_mesh_angle.faces], c="red"),
-                        *mesh_estimates
-                    )
+                        plt = Plotter()
+                        source_mesh_angle = source_mesh.copy().apply_transform(
+                            trimesh.transformations.rotation_matrix(angle, [0, 0, 1]))
+                        plt.at(0).show(
+                            Points(pointcloud.cpu().numpy(), c="green", alpha=0.2),
+                            Points(free_pointcloud.cpu().numpy(), c="blue", alpha=0.2),
+                            Mesh([source_mesh_angle.vertices, source_mesh_angle.faces], c="red"),
+                            *mesh_estimates
+                        )
 
-                results[partial_fn][angle] = {
-                    "res": res,
-                    "all_solutions": all_solutions
-                }
+                    results[partial_fn][angle] = {
+                        "res": res,
+                        "all_solutions": all_solutions
+                    }
 
-        # Save results.
-        mmint_utils.save_gzip_pickle(results, os.path.join(plausibles_dir, mesh_fn + ".pkl"))
+                    pbar.update()
+
+            # Save results.
+            mmint_utils.save_gzip_pickle(results, os.path.join(plausibles_dir, mesh_fn + ".pkl"))
 
 
 if __name__ == '__main__':
