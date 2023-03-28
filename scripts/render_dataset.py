57f5ec6a8106cde72083e553e3d3a5b0c041427e
Mark Van der Merwe <markvdm@umich.edu>
2023-03-28 18:21:30 -0400

    New rendering with pytorch kinematics and added SDF check.

--- scripts/render_dataset.py
+++ scripts/render_dataset.py
@@ -4,18 +4,24 @@ import os
 import mmint_utils
 import numpy as np
 import pyrender
+import torch
 import trimesh
 import transforms3d as tf3d
+from scripts.generate_sdf import get_sdf_values
 from tqdm import tqdm
 from usdf.utils import utils
 from usdf.utils.render_utils import depth_to_pointcloud, depth_to_free_points
 from vedo import Plotter, Points, Mesh
+import pytorch_volumetric as pv
+import pytorch_kinematics as pk
 
 os.environ["PYOPENGL_PLATFORM"] = "egl"
 
 
 def render_dataset(dataset_cfg: dict, split: str, vis: bool = False):
+    dtype = torch.float
     N = 8  # Number of angles to render mesh from.
+    free_space_surface_epsilon = 0.01
 
     meshes_dir = dataset_cfg["meshes_dir"]
     dataset_dir = dataset_cfg["dataset_dir"]
@@ -38,6 +44,7 @@ def render_dataset(dataset_cfg: dict, split: str, vis: bool = False):
     camera_pose[:3, :3] = tf3d.euler.euler2mat(np.pi / 2.0, 0, 0, axes="sxyz")
     camera_pose[:3, 3] = [0, -2.0, 0]
     scene.add(camera, pose=camera_pose)
+    camera_to_world_tf = pk.Transform3d(matrix=torch.tensor(camera_pose, dtype=dtype))
 
     # Add light to the scene.
     light = pyrender.SpotLight(color=np.ones(3), intensity=3.0, innerConeAngle=np.pi / 16.0, outerConeAngle=np.pi / 6.0)
@@ -49,6 +56,7 @@ def render_dataset(dataset_cfg: dict, split: str, vis: bool = False):
     with tqdm(total=len(meshes) * N) as pbar:
         for mesh_fn in meshes:
             mesh_path = os.path.join(meshes_dir, mesh_fn)
+            obj = pv.MeshObjectFactory(mesh_path)
             mesh_tri: trimesh.Trimesh = trimesh.load(mesh_path)
             mesh = pyrender.Mesh.from_trimesh(mesh_tri)
             mesh_node = pyrender.Node(mesh=mesh, matrix=np.eye(4))
@@ -58,9 +66,10 @@ def render_dataset(dataset_cfg: dict, split: str, vis: bool = False):
             mmint_utils.make_dir(mesh_partials_path)
 
             for angle_idx, angle in enumerate(np.linspace(0, 2 * np.pi, N + 1)[:-1]):
-                object_pose = np.eye(4)
-                object_pose[:3, :3] = tf3d.euler.euler2mat(0, 0, angle, axes="sxyz")
-                scene.set_pose(mesh_node, object_pose)
+                object_to_world_tf = pk.RotateAxisAngle(angle, "Z", dtype=dtype)
+                # object_pose = np.eye(4)
+                # object_pose[:3, :3] = tf3d.euler.euler2mat(0, 0, angle, axes="sxyz")
+                scene.set_pose(mesh_node, object_to_world_tf.get_matrix().cpu().numpy()[0])
 
                 # Render the scene.
                 color, depth = r.render(scene)
@@ -70,21 +79,31 @@ def render_dataset(dataset_cfg: dict, split: str, vis: bool = False):
                 pointcloud = utils.transform_pointcloud(pointcloud, camera_pose)
 
                 # Recover free points.
-                free_pointcloud = depth_to_free_points(depth, yfov, max_depth=3.0, n=200)
-                free_pointcloud = utils.transform_pointcloud(free_pointcloud, camera_pose)
+                free_pointcloud_camera = depth_to_free_points(depth, yfov, max_depth=3.0, n=200)[:, :3]
+                free_pointcloud_world = camera_to_world_tf.transform_points(
+                    torch.tensor(free_pointcloud_camera, dtype=dtype))
+
+                world_to_object_tf = object_to_world_tf.inverse()
+                free_pointcloud_obj = world_to_object_tf.transform_points(free_pointcloud_world).cpu().numpy()
+                sdn = obj.object_frame_closest_point(free_pointcloud_obj).distance
+
+                free_pointcloud_world = free_pointcloud_world[sdn > free_space_surface_epsilon]
+                free_pointcloud_world = free_pointcloud_world.cpu().numpy()
 
                 if vis:
+                    mesh_tri_rot = mesh_tri.copy().apply_transform(object_to_world_tf.get_matrix().cpu().numpy()[0])
+
                     plt = Plotter()
-                    mesh_tri_rot = mesh_tri.copy().apply_transform(object_pose)
                     plt.at(0).show(Points(pointcloud[:, :3], c="green"),
-                                   Mesh([mesh_tri_rot.vertices, mesh_tri_rot.faces]),
-                                   Points(free_pointcloud[:, :3], c="blue", alpha=0.1))
+                                   Mesh([mesh_tri_rot.vertices, mesh_tri_rot.faces], alpha=0.5),
+                                   Points(free_pointcloud_world[:, :3], c="blue", alpha=1.0))
 
                 # Save partials.
                 mesh_partials_angle_path = os.path.join(mesh_partials_path, "angle_%d" % angle_idx)
                 mmint_utils.make_dir(mesh_partials_angle_path)
                 utils.save_pointcloud(pointcloud, os.path.join(mesh_partials_angle_path, "pointcloud.ply"))
-                utils.save_pointcloud(free_pointcloud, os.path.join(mesh_partials_angle_path, "free_pointcloud.ply"))
+                utils.save_pointcloud(free_pointcloud_world,
+                                      os.path.join(mesh_partials_angle_path, "free_pointcloud.ply"))
                 mmint_utils.save_gzip_pickle({"angle": angle}, os.path.join(mesh_partials_angle_path, "info.pkl.gzip"))
 
                 pbar.update(1)
